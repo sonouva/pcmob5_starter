1d912f810bf6f22b00aa706635a520e53cb13be0
junjie <jun_jie1994@hotmail.com>
2021-01-08 11:46:55 +0800

    async

--- App.js
+++ App.js
@@ -5,13 +5,35 @@ import SignInScreen from "./screens/SignInScreen";
 import AccountScreen from "./screens/AccountScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect } from "react";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
+
+  const [loading, setLoading] = useState(true);
+  const [signedIn, setSignedIn] = useState(false);
+
+  async function loadToken() {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      setSignedIn(true)
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadToken();
+  },[]);
+
+  return loading ? (
+    <View style={styles.container}>
+      <ActivityIndicator/>
+    </View>
+  ):(
     <NavigationContainer>
-      <Stack.Navigator mode="modal" headerMode="none">
+      <Stack.Navigator mode="modal" headerMode="none" initialRouteName = {signedIn ? "Account" : "SignedIn"}>
         <Stack.Screen component={AccountScreen} name="Account" />
         <Stack.Screen component={SignInScreen} name="SignIn" />
       </Stack.Navigator>
